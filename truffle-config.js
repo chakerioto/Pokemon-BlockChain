a018a2e7458209b89469e9cafb8d88138c1f5a03
THANGNQ <chakerioto@gmail.com>
2021-03-01 17:10:37 +0700

    5

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,22 @@
-const path = require("path");
+const path = require('path');
+var HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          MNENOMIC,
+          'https://ropsten.infura.io/v3/' + INFURA_API_KEY
+        ),
+      network_id: 3,
+      gas: 4612388,
+    },
+  },
 };
