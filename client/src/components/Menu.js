75163486cf52c6662fc74d311da2408f02c1aa5c
THANGNQ <chakerioto@gmail.com>
2021-03-01 14:33:55 +0700

    first pokemon

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function Menu() {
+function Menu({ setState }) {
+  const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
   const [menuList, setMenuList] = useState([
     'Create New Pokemon',
     'Attack',
     'Market Place',
   ]);
+  const [currentPokemon, setCurrentPokemon] = useState({});
 
-  const handleClick = el => {
+  const handleClick = async el => {
     switch (el) {
       case 'Create New Pokemon':
-        alert('hihi');
-        //Create pokemon here :
-        // Xai axios get pokemon
+        const randomNumber = Math.floor(Math.random() * 100 + 1);
+        const response = await axios.get(baseUrl + randomNumber);
+        const name = response.data.name;
+        const hp = response.data.stats[0].base_stat;
+        const attack = response.data.stats[1].base_stat;
+        const imgUrl = response.data.sprites.front_default;
+
         // Luu vao database pokemon
         // get pokemon ra set Pokemon list o app.js
         // va hien no ra o MyPokemonList
